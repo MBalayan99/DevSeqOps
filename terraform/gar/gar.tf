c2b5f75cce9aef011b90a006083b1c857b956275
maneav78 <avoyan.mane@mail.ru>
2025-04-11 22:54:17 +0400

    draft commit

--- terraform/gar/gar.tf
+++ terraform/gar/gar.tf
@@ -4,4 +4,8 @@ resource "google_artifact_registry_repository" "my-repo" {
   repository_id = "docker-repo"
   description   = "Docker repository for images"
   format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
 }
--- terraform/genopenvpn.sh
+++ terraform/genopenvpn.sh
@@ -1,119 +1,85 @@
 #!/bin/bash
+set -euo pipefail
 
-set -e
+# Install OpenVPN and Easy-RSA
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt install -y openvpn easy-rsa ufw
 
-# Variables
-VPN_USER="developer1"
-VPN_SERVER_NAME="server"
-VPN_SUBNET="10.8.0.0"
-VPN_SUBNET_MASK="255.255.255.0"
-EXTERNAL_IP=$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google")
-IFACE="ens4" # May vary (eth0 on some)
+# Set up Easy-RSA
+EASYRSA_DIR=/etc/openvpn/easy-rsa
+make-cadir $EASYRSA_DIR
+cd $EASYRSA_DIR
+export EASYRSA_BATCH=1
 
-# Update & install
-apt-get update
-apt-get install -y openvpn easy-rsa ufw curl
-
-# Easy-RSA setup
-make-cadir /etc/openvpn/easy-rsa
-cd /etc/openvpn/easy-rsa
 ./easyrsa init-pki
-echo -ne '\n' | ./easyrsa build-ca nopass
-./easyrsa gen-req $VPN_SERVER_NAME nopass
-./easyrsa sign-req server $VPN_SERVER_NAME <<< "yes"
+./easyrsa build-ca nopass
+./easyrsa gen-req server nopass
+./easyrsa sign-req server server
 ./easyrsa gen-dh
-openvpn --genkey --secret ta.key
-./easyrsa gen-req $VPN_USER nopass
-./easyrsa sign-req client $VPN_USER <<< "yes"
+openvpn --genkey secret ta.key
 ./easyrsa gen-crl
 
-# Copy server files
+./easyrsa gen-req developer1 nopass
+./easyrsa sign-req client developer1
+
+# Move files to /etc/openvpn
 cp pki/ca.crt /etc/openvpn/
-cp pki/issued/$VPN_SERVER_NAME.crt /etc/openvpn/server.crt
-cp pki/private/$VPN_SERVER_NAME.key /etc/openvpn/server.key
+cp pki/issued/server.crt /etc/openvpn/
+cp pki/private/server.key /etc/openvpn/
 cp pki/dh.pem /etc/openvpn/
 cp ta.key /etc/openvpn/
 cp pki/crl.pem /etc/openvpn/
 
-# Create server.conf
+# Write OpenVPN server configuration
 cat > /etc/openvpn/server.conf <<EOF
 port 1194
 proto udp
 dev tun
-ca ca.crt
-cert server.crt
-key server.key
-dh dh.pem
+ca /etc/openvpn/ca.crt
+cert /etc/openvpn/server.crt
+key /etc/openvpn/server.key
+dh /etc/openvpn/dh.pem
 auth SHA256
-tls-auth ta.key 0
-topology subnet
-server $VPN_SUBNET $VPN_SUBNET_MASK
-push "redirect-gateway def1 bypass-dhcp"
-push "dhcp-option DNS 8.8.8.8"
-push "dhcp-option DNS 8.8.4.4"
-keepalive 10 120
+tls-auth /etc/openvpn/ta.key 0
 cipher AES-256-CBC
 user nobody
 group nogroup
 persist-key
 persist-tun
+keepalive 10 120
+topology subnet
+server 10.8.0.0 255.255.255.0
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
 status /var/log/openvpn-status.log
 log /var/log/openvpn.log
 verb 3
-crl-verify crl.pem
+crl-verify /etc/openvpn/crl.pem
 EOF
 
 # Enable IP forwarding
-echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
-# UFW Rules
+# Configure UFW
 ufw allow 1194/udp
 ufw allow OpenSSH
-sed -i "s/^DEFAULT_FORWARD_POLICY=.*/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/" /etc/default/ufw
 
-cat > /etc/ufw/before.rules <<EOF
-*nat
-:POSTROUTING ACCEPT [0:0]
--A POSTROUTING -s $VPN_SUBNET/$VPN_SUBNET_MASK -o $IFACE -j MASQUERADE
-COMMIT
-EOF
+# Modify UFW before.rules
+sed -i '/^*filter/i \
+*nat\n\
+:POSTROUTING ACCEPT [0:0]\n\
+-A POSTROUTING -s 10.8.0.0/8 -o ens4 -j MASQUERADE\n\
+COMMIT\n' /etc/ufw/before.rules
 
+# Set forwarding policy
+sed -i 's/^DEFAULT_FORWARD_POLICY=.*/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw
+
+ufw disable
 ufw --force enable
 
 # Enable and start OpenVPN
 systemctl enable openvpn@server
 systemctl start openvpn@server
-
-# Create client .ovpn config
-mkdir -p /etc/openvpn/client-configs
-cat > /etc/openvpn/client-configs/$VPN_USER.ovpn <<EOF
-client
-dev tun
-proto udp
-remote $EXTERNAL_IP 1194
-resolv-retry infinite
-nobind
-persist-key
-persist-tun
-remote-cert-tls server
-cipher AES-256-CBC
-auth SHA256
-key-direction 1
-verb 3
-
-<ca>
-$(cat /etc/openvpn/ca.crt)
-</ca>
-<cert>
-$(cat pki/issued/$VPN_USER.crt)
-</cert>
-<key>
-$(cat pki/private/$VPN_USER.key)
-</key>
-<tls-auth>
-$(cat /etc/openvpn/ta.key)
-</tls-auth>
-EOF
-
-echo "✅ VPN setup complete. .ovpn file is at /etc/openvpn/client-configs/$VPN_USER.ovpn"
