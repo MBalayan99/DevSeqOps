186cf30392de3559f55f36db61698b5ff70ca13a
Mher <48990582+MBalayan99@users.noreply.github.com>
2025-04-12 22:34:47 +0400

    Update iam_bucket.tf

--- terraform/iam/iam_bucket.tf
+++ terraform/iam/iam_bucket.tf
@@ -1,3 +1,4 @@
+##iam for teraform state bucket
 resource "google_storage_bucket_iam_member" "terraform_state_object_admin" {
   bucket = "deadevsecopse-2025"
   role   = "roles/storage.objectAdmin"
