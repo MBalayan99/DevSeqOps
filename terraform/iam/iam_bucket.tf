619607679367d70cd214db40f3041481e8eea19d
mher <mher@localhost.localdomain>
2025-04-12 21:27:46 +0400

    add permition for bucket

---
+++ terraform/iam/iam_bucket.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket_iam_member" "terraform_state_object_admin" {
+  bucket = "deadevsecopse-2025"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:git-572@devsecops-454508.iam.gserviceaccount.com"
+}
