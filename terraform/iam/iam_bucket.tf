f7487cbd4fea04087126e1b5a62df511f6285774
Mher <48990582+MBalayan99@users.noreply.github.com>
2025-04-13 13:05:56 +0400

    disable iam_bucket.tf

--- terraform/iam/iam_bucket.tf
+++ terraform/iam/iam_bucket.tf
@@ -1,6 +1,6 @@
 ##iam for teraform state bucket
-resource "google_storage_bucket_iam_member" "terraform_state_object_admin" {
-  bucket = "deadevsecopse-2025"
-  role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:git-572@devsecops-454508.iam.gserviceaccount.com"
-}
+#resource "google_storage_bucket_iam_member" "terraform_state_object_admin" {
+ # bucket = "deadevsecopse-2025"
+  #role   = "roles/storage.objectAdmin"
+  #member = "serviceAccount:git-572@devsecops-454508.iam.gserviceaccount.com"
+#}
